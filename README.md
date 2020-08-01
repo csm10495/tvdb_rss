306f6e441d96a308dee3456651b3d6fe9e85d14e
Charles Machalow <cmachalow@linkedin.com>
2020-07-31 23:42:39 -0700

    add more stuffs

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A small Python library to generate an RSS of new shows (that I want to watch)
 
+# Documentation
+
+I recommend just looking at the source code for how things work. If there is community interest, I can doc-up the code better.
+
+# Install
+```
+pip install tvdb_rss
+```
+
 # License
 
 MIT License
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+import sys
+
+setup(
+    name='tvdb_rss',
+    author='csm10495',
+    author_email='csm10495@gmail.com',
+    url='https://github.com/csm10495/tvdb_rss',
+    version='0.0.1',
+    packages=['tvdb_rss'],
+    license='MIT License',
+    python_requires='>=3.7',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    include_package_data = True,
+    install_requires=['csmlog', 'requests', 'feedgen'],
+)
