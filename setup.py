143a86a1d8c62c51e5a75bd2ab40c26523b5e2f5
Charles Machalow <csm10495@gmail.com>
2020-08-26 17:51:13 -0700

    upversion

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='csm10495',
     author_email='csm10495@gmail.com',
     url='https://github.com/csm10495/tvdb_rss',
-    version='0.0.11',
+    version='0.0.12',
     packages=['tvdb_rss'],
     license='MIT License',
     python_requires='>=3.7',
